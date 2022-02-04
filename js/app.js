a886c487738392f6d0c9da89df69644e6886b103
LukeSchaffel <lukeschaffel@gmail.com>
2022-02-03 20:48:14 -0500

    link board array to dom

--- js/app.js
+++ js/app.js
@@ -12,36 +12,43 @@ let winner
 
 
 /*------------------------ Cached Element References ------------------------*/
-let topLeft = document.getElementById('sq0')
-let topMid = document.getElementById('sq1')
-let topRight = document.getElementById('sq2')
-let midLeft = document.getElementById('sq3')
-let midMid = document.getElementById('sq4')
-let midRight = document.getElementById('sq5')
-let bottomLeft = document.getElementById('sq6')
-let bottomMid = document.getElementById('sq7')
-let bottomRight = document.getElementById('sq8')
+// let topLeft = document.getElementById('sq0')
+// let topMid = document.getElementById('sq1')
+// let topRight = document.getElementById('sq2')
+// let midLeft = document.getElementById('sq3')
+// let midMid = document.getElementById('sq4')
+// let midRight = document.getElementById('sq5')
+// let bottomLeft = document.getElementById('sq6')
+// let bottomMid = document.getElementById('sq7')
+// let bottomRight = document.getElementById('sq8')
 
-let board = document.querySelectorAll('.board-square')
+let boardEl = document.querySelectorAll('.board-square')
 
 let gameMessage = document.getElementById('message')
 
+
 /*----------------------------- Event Listeners -----------------------------*/
+
+  
+  
+
+/*-------------------------------- Functions --------------------------------*/
 function init() {
-  gameBoard = [null,null,null,null,null,null,null,null,null ]
+  gameBoard = [1,-1,1,-1,null,null,null,null,null ]
   //console.log(gameBoard)
  
-  gameBoard.map(function (sq, i) {
-
-    board = sq
-    console.log(board)
+  // gameBoard.map(function (sq, i) {
+  //   boardEl.item[i] = sq
+  //   //console.log(boardEl)
     
-  })
-
-
+  //   // console.log(boardEl)
+  //   return boardEl
+  // })
+  // console.log(boardEl.item[0])
+    
   // topLeft = gameBoard[0]
-  // topRight = gameBoard[1]
-  // topMid = gameBoard[2]
+  // topMid = gameBoard[1]
+  // topRight = gameBoard[2]
   // midLeft = gameBoard[3]
   // midMid = gameBoard[4]
   // midRight = gameBoard[5]
@@ -49,13 +56,41 @@ function init() {
   // bottomMid = gameBoard[7]
   // bottomRight = gameBoard[8]
 
+  turn = 1
+  winner = null
+
+  render()
   
   }
   
+  init()
+
+function render() {
+checkGameBoard()  
+renderMessage()
 
 
+}
+
+function checkGameBoard() {
+  gameBoard.forEach(function (square, i) {
+    if (square === 1) {
+     boardEl[i].textContent = "X" 
+     boardEl[i].style.color = "red"
+     boardEl[i].style.backgroundColor = "white"
+    } else if (square === -1){
+      boardEl[i].textContent = 'O'
+      boardEl[i].style.color = "Green"
+      boardEl[i].style.backgroundColor = "white"
+    }
+    })
+}
 
 
-/*-------------------------------- Functions --------------------------------*/
-
+function renderMessage() {
+  if (winner === null) {
+    gameMessage.textContent = `${turn ===1 ? `It's Player 1's Turn! Place your "X" ` : `It is player 2's turn! Place Your "O"` }`
+  }
+ 
+}
 
